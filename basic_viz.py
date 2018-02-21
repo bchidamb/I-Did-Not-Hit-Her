ae7dc7c0e31820bd7bcf9073f0a0140739c7033f
Connor Soohoo <connorsoohoo@gmail.com>
2018-02-21 12:13:29 -0800

    modded visualization for highest rated

--- basic_viz.py
+++ basic_viz.py
@@ -64,46 +64,47 @@ if __name__ == "__main__":
     movies['avg_rating'] = 0
 
     # plot histogram for all ratings
-    plot_histogram(data['rating'], 'All Ratings')
+    #plot_histogram(data['rating'], 'All Ratings')
 
     # build data for most popular and highest rating
-    # for i in range(1, 1682+1):
-    #     # Get all movie ratings for a specific movies
-    #     movie_ratings = fetch_all_ratings(data, i)
-    #
-    #     # Compute the number of ratings and average ratings
-    #     ri_avg = np.mean(ri)
-    #     ri_num = len(ri)
-    #
-    #     # Add these computed values to our DataFrame
-    #     movies.loc[movies['movie_id'] == i, 'num_ratings']= ri_num
-    #     movies.loc[movies['movie_id'] == i, 'avg_rating']= ri_avg
-    #
-    #
-    # # Plot most popular (highest number) ratings
-    # movies.sort_values('num_ratings', inplace=True, ascending=False)
-    # popular_movie_ids = movies[:10]['movie_id'].values
-    # total_pop_ratings = []
-    # for pop_id in popular_movie_ids:
-    #     pop_ratings = data.loc[data['movie_id'] == pop_id, 'rating']
-    #     total_pop_ratings.append(pop_ratings)
-    #
-    # total_pop_ratings = pd.Series(np.array(total_pop_ratings))
-    #
-    # plot_histogram(total_pop_ratings, 'Most Popular', filename='4_2',save=True)
-    #
-    #
-    # # Plot highest rated movies
-    # movies.sort_values('avg_rating', inplace=True, ascending=False)
-    # highest_rated_movie_ids = movies[:10]['movie_id'].values
-    # total_high_ratings = []
-    # for high_id in highest_rated_movie_ids:
-    #     high_ratings = data.loc[data['movie_id'] == high_id, 'rating']
-    #     total_high_ratings.append(high_ratings)
-    #
-    # total_high_ratings = pd.Series(np.array(total_high_ratings))
-    #
-    # plot_histogram(total_high_ratings, 'Highest Rated', filename='4_3',save=True)
+    for i in range(1, 1682+1):
+        # Get all movie ratings for a specific movies
+        movie_ratings = fetch_all_ratings(data, i)
+
+        # Compute the number of ratings and average ratings
+        ri_avg = np.mean(movie_ratings)
+        ri_num = len(movie_ratings)
+
+        # Add these computed values to our DataFrame
+        movies.loc[movies['movie_id'] == i, 'num_ratings']= ri_num
+        movies.loc[movies['movie_id'] == i, 'avg_rating']= ri_avg
+
+    print("got here")
+
+    # Plot most popular (highest number) ratings
+    movies.sort_values('num_ratings', inplace=True, ascending=False)
+    popular_movie_ids = movies[:10]['movie_id'].values
+    total_pop_ratings = []
+    for pop_id in popular_movie_ids:
+        pop_ratings = data.loc[data['movie_id'] == pop_id, 'rating']
+        total_pop_ratings.append(pop_ratings)
+
+    total_pop_ratings = pd.Series(np.array(total_pop_ratings))
+
+    #plot_histogram(total_pop_ratings, 'Most Popular', filename='4_2',save=True)
+
+
+    # Plot highest rated movies
+    movies.sort_values('avg_rating', inplace=True, ascending=False)
+    highest_rated_movie_ids = movies[:10]['movie_id'].values
+    total_high_ratings = []
+    for high_id in highest_rated_movie_ids:
+        high_ratings = data.loc[data['movie_id'] == high_id, 'rating']
+        total_high_ratings.append(high_ratings)
+
+    total_high_ratings = pd.Series(np.array(total_high_ratings))
+
+    plot_histogram(total_high_ratings, 'Highest Rated', filename='4_3',save=True)
 
     # Example for saving plot:
     # plot_histogram(data['rating'], 'All Ratings', filename='4_1', save=True)
